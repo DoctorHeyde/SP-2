13702a33d9573d22381036b9405f9b89d8ded105
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-04-02 12:32:26 +0200

    Entities has been implemented. Login and register works

--- src/main/java/app/persistance/IUserDAO.java
+++ src/main/java/app/persistance/IUserDAO.java
@@ -4,8 +4,10 @@ import app.entities.Role;
 import app.entities.User;
 import app.exceptions.EntityNotFoundException;
 
+import java.util.Set;
+
 public interface IUserDAO {
-    public User createUser(String username, String password);
+    public User createUser(String email, String password, String name, int phoneNumber, Set<String> roles);
     public User verifyUser(String username, String password) throws EntityNotFoundException;
     public User addRoleToUser(String username, String role) throws EntityNotFoundException;
     public Role createRole(String role);
