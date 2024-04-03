4e8f5398738ab6e006d755e892fc92c7d8288c68
DavidAH <cph-dh223@cphbusiness.dk>
2024-04-03 13:19:40 +0200

    Update EventDTO.java
    made its so dto dont have local date

--- src/main/java/app/dtos/EventDTO.java
+++ src/main/java/app/dtos/EventDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import app.entities.Event;
 import app.entities.Status;
@@ -13,6 +14,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+ 
 
 @Getter
 @Setter 
@@ -24,7 +30,8 @@ public class EventDTO {
     private String title;
     private String startTime;
     private String description;
-    private LocalDate dateOfEvent;
+
+    private String dateOfEvent;
     private int durationInHours;
     private int maxNumberOfStudents;
     private String locationOfEvent;
@@ -33,17 +40,17 @@ public class EventDTO {
     private String category;
     private String image;
     private Status status;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private LocalDateTime canceledAt;
-    private Set<User> users;
+    private String createdAt;
+    private String updatedAt;
+    private String canceledAt;
+    private Set<UserDTO> users;
 
     public EventDTO(Event event) {
         this.id = event.getId();
         this.title = event.getTitle();
         this.startTime = event.getStartTime();
         this.description = event.getDescription();
-        this.dateOfEvent = event.getDateOfEvent();
+        this.dateOfEvent = event.getDateOfEvent().toString();
         this.durationInHours = event.getDurationInHours();
         this.maxNumberOfStudents = event.getMaxNumberOfStudents();
         this.locationOfEvent = event.getLocationOfEvent();
@@ -52,9 +59,9 @@ public class EventDTO {
         this.category = event.getCategory();
         this.image = event.getImage();
         this.status = event.getStatus();
-        this.createdAt = event.getCreatedAt();
-        this.updatedAt = event.getUpdatedAt();
-        this.canceledAt = event.getCanceledAt();
-        this.users = event.getUsers();
+        this.users = event.getUsers().stream().map(u -> new UserDTO(u)).collect(Collectors.toSet());
+        if(event.getCreatedAt() != null) this.createdAt = event.getCreatedAt().toString();
+        if(event.getUpdatedAt() != null) this.updatedAt = event.getUpdatedAt().toString();
+        if(event.getCanceledAt() != null) this.canceledAt = event.getCanceledAt().toString();
     }
 }
